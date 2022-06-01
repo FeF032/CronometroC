c2f7b3fe513d93f2fc10b820c3ff4cae3e1ad455
EnryTof <105856023+EnryTof@users.noreply.github.com>
2022-06-01 12:48:16 +0200

    Update cronometro.cpp

--- cronometro.cpp
+++ cronometro.cpp
@@ -1,74 +1,47 @@
 #include<iostream>
+
 #include <time.h>
+
 using namespace std;
-class cronometro
-{
-	private: 
-	
-	char scelta;
-	
-	public: 
-	void setScelta(char s)
-	{
-	scelta=s;
+class cronometro {
+  private:
+
+    char scelta;
+
+  public:
+    void setScelta(char s) {
+      scelta = s;
     }
-    char getScelta()
-    {
-    	return scelta;
-	}
-	
-	cronometro(char s)
-	{
-		setScelta(s);
-	}
-    ~cronometro()
-	{
-		cout<<endl<<"oggetto distrutto ";
-	}
-	int avvia();
-	int stoppa();
+  char getScelta() {
+    return scelta;
+  }
+
+  cronometro(char s) {
+    setScelta(s);
+  }~cronometro() {
+    cout << endl << "oggetto distrutto ";
+  }
+  int avvia();
+  int stoppa();
 };
 
-int cronometro::avvia()
-{
-	if(scelta=='i' or scelta=='I')
-	{
-		cout<<endl<<"Misurazione avviata";
-		return time(NULL);
-	}
+int main() {
+  char scelta;
+  do {
+    cout << endl << "Inserisci i/I per avviare il cronometro: ";
+    cin >> scelta;
+  }
+  while (scelta != 'i' && scelta != 'I');
+  cronometro c(scelta);
+  int inizio = c.avvia();
+  do {
+    cout << endl << "Inserisci f/F per fermare il cronometro: ";
+    cin >> scelta;
+  }
+  while (scelta != 'f' && scelta != 'F');
+  c.setScelta(scelta);
+  cout << endl << "Tempo trascorso: " << c.stoppa() - inizio << " secondi";
+  return 0;
 }
-
-int cronometro::stoppa()
-{
-	cout<<endl<<"Indirizzo di memoria: "<<this;
-	if(scelta=='f' || scelta=='F')
-	{	
-	cout<<endl<<"Misurazione terminata";
-	return time(NULL);	
-	}
-}
-
-int main()
-{
-	char scelta;
-do
-	{
-	cout<<endl<<"Inserisci i/I per avviare il cronometro: ";
-	cin>>scelta;
-	}
-while (scelta!='i' && scelta!='I');
-cronometro c(scelta);
-int inizio=c.avvia();
-do
-	{
-	cout<<endl<<"Inserisci f/F per fermare il cronometro: ";
-	cin>>scelta;
-	}
-while (scelta!='f' && scelta!='F');
-c.setScelta(scelta);
-cout<<endl<<"Tempo trascorso: "<<c.stoppa()-inizio<<" secondi"; 	
-return 0;
-}
-
 
 
